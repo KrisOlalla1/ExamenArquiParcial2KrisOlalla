e3f449ea45cc21bb94c81dcf1c1472068254e4c9
KrisOlalla1 <ksolalla@espe.edu.ec>
2025-12-17 20:04:18 -0500

    Agregar clase principal Spring Boot

---
+++ src/main/java/com/examen/branches_api/BranchesApiApplication.java
@@ -0,0 +1,13 @@
+package com.examen.branches_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchesApiApplication.class, args);
+	}
+
+}
